33a31ed054c1271b4ab3e73bb9513d4bd5fe5c94
Raissa-Reis-Lopes <raissa.vilemon@gmail.com>
2023-03-22 18:51:37 -0300

    Ajuste no JPAFactory

--- src/main/java/br/com/hotel/devaneio/util/JPAFactory.java
+++ src/main/java/br/com/hotel/devaneio/util/JPAFactory.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAFactory {
 
-	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("hotel1800");
+	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("hotel-devaneio");
 
 	public static EntityManager getEntityManager() {
 		return factory.createEntityManager();
